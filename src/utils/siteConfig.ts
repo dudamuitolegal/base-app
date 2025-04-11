b52cad23c4a0dab5e11bf725ff5c609961fda374
duda <78374716+dudamuitolegal@users.noreply.github.com>
2025-04-11 13:40:21 -0300

    Update siteConfig.ts

--- src/utils/siteConfig.ts
+++ src/utils/siteConfig.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
     name: "468bet9",
     url: "https://468bet9.com.br",
     analytics: "",
-    defaultLink: "https://go.aff.upbet.com/c54a8g7o",
+    defaultLink: "",
     logo: "/logo.avif",
     logoWidth: 150,
     logoHeight: 150,
