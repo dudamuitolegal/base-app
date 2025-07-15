6cb85535b7a9cd7f41313f82b6becaa45485f994
Seu Nome <dudado12lol@gmail.com>
2025-07-15 18:29:04 -0300

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,9 @@
 import type { NextConfig } from "next";
-import {siteConfig} from "@/utils/siteConfig";
 
 const nextConfig: NextConfig = {
     output: "export",
     distDir: "out",
     images: {unoptimized: true},
-    async redirects(){
-        return [
-            {
-                source: "/go/:path*",
-                destination: siteConfig.affiliateLink,
-                permanent: false,
-            }
-        ]
-    }
 };
 
 export default nextConfig;
